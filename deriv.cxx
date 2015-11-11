410a9b0702b60b6bfe9076aae4c84924243af6e0
@b1e9n9e0 <benedikt.schlosza@hhu.de>
2015-11-11 17:15:21 +0100

    lab4 done, yet uncommented

--- deriv.cxx
+++ deriv.cxx
@@ -4,12 +4,35 @@
 using namespace std;
 
 
-void print(double* p, const int N, const double dx,
-           const double xmin)
+void print(double* p, const int N, const double dx,const double xmin)
 {
    for(int i=0; i<N; i++)
        cout << i*dx + xmin << "\t" << p[i] << endl;
+
 }
+
+double* fillarray(double* p, const int N, const double dx,const double xmin){
+	double a = 0;
+	for(int i = 0; i < N; i++){
+		a = xmin + (i * dx);
+		p[i] = exp(-(a*a));
+
+	}
+
+}
+
+double* derive(double* p, const int N, const double dx){
+	double xim1 = 0;
+	double xip1 = p[1];
+	double xtemp = 0;
+	for(int i = 0; i < N ; i++){
+		xtemp = p[i];
+		p[i] = (xip1-xim1)/(2.*dx);
+		xim1 = xtemp;
+		xip1 = p[i+2];
+}
+}
+
 
 
 int main(){
@@ -19,8 +42,14 @@ int main(){
   const double xmin = -15;
   const double dx = (xmax-xmin)/(N-1);
 
+
+
   // call to function which fills array p here
+  fillarray(p,N,dx,xmin);
   // call to functio which calculates the derivative
+  derive(p,N,dx);
+
+
   print(p,N,dx,xmin);
 
   return 0;
